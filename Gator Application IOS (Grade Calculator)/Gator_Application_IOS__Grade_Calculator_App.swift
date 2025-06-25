473ce1825d0a1fbea89acdec5b13e0437bd58eba
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-06-25 21:36:19 +0700

    Initial Commit

---
+++ Gator Application IOS (Grade Calculator)/Gator_Application_IOS__Grade_Calculator_App.swift
@@ -0,0 +1,17 @@
+//
+//  Gator_Application_IOS__Grade_Calculator_App.swift
+//  Gator Application IOS (Grade Calculator)
+//
+//  Created by Foundation-017 on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Gator_Application_IOS__Grade_Calculator_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gator Application IOS (Grade Calculator)Tests/Gator_Application_IOS__Grade_Calculator_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Gator_Application_IOS__Grade_Calculator_Tests.swift
+//  Gator Application IOS (Grade Calculator)Tests
+//
+//  Created by Foundation-017 on 25/06/25.
+//
+
+import Testing
+@testable import Gator_Application_IOS__Grade_Calculator_
+
+struct Gator_Application_IOS__Grade_Calculator_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
